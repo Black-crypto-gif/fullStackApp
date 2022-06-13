f1c12e9f6db576b5894b4cf18c4dc8b9d19852cb
MELLAL Fethi <fethimellal@gmail.com>
2022-06-13 03:05:25 +0200

    v1.0.2

---
+++ client/src/components/AddProjectModal.js
@@ -0,0 +1,117 @@
+import React from 'react'
+import { useState } from 'react'
+import { useMutation, useQuery } from '@apollo/client'
+/* import { GET_PROJECTS } from '../queries/ProjectQuery' */
+import { GET_CLIENTS } from '../queries/ClientQuery'
+import { FaList } from 'react-icons/fa'
+import swal from 'sweetalert'
+
+
+export default function AddProjectModal() {
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [clientId, setClientId] = useState('');
+    const [status, setStatus] = useState('new');
+
+    // get client for select
+    const { loading, error, data } = useQuery(GET_CLIENTS);
+
+    const onSubmit = (e) =>{
+        e.preventDefault();
+        if(name ==='' || description === '' || status === ''){
+            swal('Error', 'Please fill all the fields', 'error')
+        }
+        /* addClient(name, email, phone); */
+        setName('');
+        setDescription('');
+        setStatus('');
+    };
+    if(loading) return null;
+    if(error) return 'Something went wrong';
+    return (
+        <>
+            {!loading && !error && (
+                <>
+                <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addProjectModal">
+                <div className="d-flex align-items center">
+                    <FaList className='icon'/>
+                    <div>
+                        New Project
+                    </div>
+                </div>
+            </button>
+
+
+            <div className="modal fade" id="addProjectModal" aria-labelledby="addClientModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="addClientModalLabel">Add Project</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <form onSubmit={onSubmit}>
+                                <div className="mb-3">
+                                    <label htmlFor="" className="form-label"></label>
+                                    <input type="text" className="form-control"
+                                    id='name'
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                    placeholder="Name" />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="" className="form-label"></label>
+                                    <textarea className="form-control"
+                                    id='description'
+                                    value={description}
+                                    onChange={(e) => setDescription(e.target.value)}
+                                    placeholder="Description">
+                                    </textarea>
+                                    
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="" className="form-label">Status</label>
+                                    <select  id="status" className="form-select" value={status}
+                                    onChange={(e)=> setStatus(e.target.value)}>
+                                        <option value="new">Not Started</option>
+                                        <option value="progress">In Progress</option>
+                                        <option value="completed">Completed</option>
+                                    </select>
+                                    
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="" className='form-label'>Client</label>
+                                    <select id="clientId" className="form-select" value={clientId} onChange={(e)=> setClientId(e.target.value)}>
+                                        <option value="">
+                                            Select Client
+                                        </option>
+                                        {data.clients.map(client => (
+                                            <option
+                                            key={client.id}
+                                            value={client.id}
+                                            >
+                                            {client.name}
+                                            </option>
+                                        )
+                                        )}
+                                    </select>
+
+                                   
+                                </div>
+                                <button type="submit"
+                                data-bs-dismiss="modal" className="btn btn-primary">
+                                    Submit
+                                </button>
+
+
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                </>
+            )}
+        </>
+    )
+}
