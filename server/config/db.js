2960fc4f0df78584e8dc11f3a18cb44ae431f9c9
MELLAL Fethi <fethimellal@gmail.com>
2022-08-18 00:21:35 +0100

    Final

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    const conn = await mongoose.connect("mongodb+srv://mellalfethi:Fethi0780795322Mellal@cluster0.btgadqg.mongodb.net/mgmt_db?retryWrites=true&w=majority");
+    const conn = await mongoose.connect("mongodb+srv://mellalfethi:Fethi0780795322Mellal@cluster0.btgadqg.mongodb.net/mgmt2_db?retryWrites=true&w=majority");
     console.log(`MongoDB Connected: ${conn.connection.host}`.bgRed.bold);
 };
 
